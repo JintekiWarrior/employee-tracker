cd0ba9c920f820d7bf8fb3054c3dd1b91d619cd7
JintekiWarrior <jintekiwarrior@gmail.com>
2022-04-13 07:07:48 -0400

    Add database for employee tracker - Add table for departments - Add seeds for table departments

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const db = require('./db/connection')
+
 const PORT = process.env.PORT || 3001
 const app = express()
 
@@ -7,6 +9,16 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+// default response for requests not found
+app.use((req, res) => {
+  res.status(404).end()
 })
+
+db.connect(err => {
+  if (err) throw err
+  console.log('Connected to database')
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  }) 
+})
+
